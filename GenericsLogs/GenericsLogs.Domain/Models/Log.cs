50985ad911bfee40133a143b76d009141f973722
pedroferreira <psouza@infisconsultoria.com.br>
2024-10-26 11:39:05 -0300

    feat: criacao model Logs

---
+++ GenericsLogs/GenericsLogs.Domain/Models/Log.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericsLogs.Domain.Models
+{
+    public class Log
+    {
+        public Guid? Id { get; set; }
+        public DateTime Date { get; set; }
+        public string UserEmail { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string Message { get; set; }
+        public string StackTrace { get; set; }
+        public string StatusCode { get; set; }
+        public string App { get; set; }
+        public string Environment { get; set; }
+    }
+}
