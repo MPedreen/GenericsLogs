2847856fa0332d0c1d0ee9e257cfee4013a53b08
pedroferreira <psouza@infisconsultoria.com.br>
2024-10-26 11:32:01 -0300

    feat: criando mongo db context e settings

---
+++ GenericsLogs/GenericsLogs.Infra/Data/Context.Mongo/MongoDbContext.cs
@@ -0,0 +1,30 @@
+﻿namespace GenericsLogs.Infra.Data.Context.Mongo
+{
+    public class MongoDbContext
+    {
+        private readonly MongoDbSettings? _mongoDbSettings;
+        private IMongoDatabase? _mongoDatabase;
+
+        public MongoDbContext(IOptions<MongoDbSettings>? mongoDbSettings)
+        {
+            _mongoDbSettings = mongoDbSettings.Value;
+
+            #region Conexão com o banco de dados
+            var client = MongoClientSettings.FromUrl(new MongoUrl(_mongoDbSettings.Host));
+
+            if (_mongoDbSettings.IsSSL)
+                client.SslSettings = new SslSettings
+                {
+                    EnabledSslProtocols = System.Security
+                    .Authentication.SslProtocols.None
+                };
+
+            _mongoDatabase = new MongoClient(client)
+                .GetDatabase(_mongoDbSettings.Name);
+            #endregion
+        }
+
+        public IMongoCollection<Log> GetCollection(string collectionName) =>
+            _mongoDatabase.GetCollection<Log>(collectionName);
+    }
+}
---
+++ GenericsLogs/GenericsLogs.Infra/Data/Context.Mongo/MongoDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace GenericsLogs.Infra.Data.Context.Mongo
+{
+    public class MongoDbSettings
+    {
+        public string Host { get; set; }
+        public string Name { get; set; }
+        public bool IsSSL { get; set; }
+    }
+}
