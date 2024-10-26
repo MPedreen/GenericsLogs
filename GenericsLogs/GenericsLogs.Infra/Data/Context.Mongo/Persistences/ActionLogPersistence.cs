0daa3268e8c493381d666f8a841a0d82d4f7185e
pedroferreira <psouza@infisconsultoria.com.br>
2024-10-26 11:29:27 -0300

    feat: criando action log persistence e erro log persistence implementacao camada mongo infra

---
+++ GenericsLogs/GenericsLogs.Infra/Data/Context.Mongo/Persistences/ActionLogPersistence.cs
@@ -0,0 +1,42 @@
+﻿namespace GenericsLogs.Infra.Data.Context.Mongo.Persistences
+{
+    public class ActionLogPersistence : IActionLogPersistence
+    {
+        private readonly MongoDbContext? _mongoDbContext;
+
+        public ActionLogPersistence(MongoDbContext? mongoDbContext)
+        {
+            _mongoDbContext = mongoDbContext;
+        }
+
+        public void Add(Log model)
+            => _mongoDbContext.GetCollection("LogsActions").InsertOne(model);
+
+        public async Task AddAsync(Log model)
+            => await _mongoDbContext.GetCollection("LogsActions").InsertOneAsync(model);
+
+        public void Delete(Log model)
+        {
+            var filter = Builders<Log>.Filter.Eq(c => c.Id, model.Id);
+            _mongoDbContext.GetCollection("LogsActions").DeleteOne(filter);
+        }
+
+        public List<Log> GetAll()
+        {
+            var filter = Builders<Log>.Filter.Where(c => true);
+            return _mongoDbContext.GetCollection("LogsActions").Find(filter).ToList();
+        }
+
+        public Log GetbyId(Guid id)
+        {
+            var filter = Builders<Log>.Filter.Eq(c => c.Id, id);
+            return _mongoDbContext.GetCollection("LogsActions").Find(filter).FirstOrDefault();
+        }
+
+        public void Update(Log model)
+        {
+            var filter = Builders<Log>.Filter.Eq(c => c.Id, model.Id);
+            _mongoDbContext.GetCollection("LogsActions").ReplaceOne(filter, model);
+        }
+    }
+}
---
+++ GenericsLogs/GenericsLogs.Infra/Data/Context.Mongo/Persistences/ErroLogPersistence.cs
@@ -0,0 +1,39 @@
+﻿namespace GenericsLogs.Infra.Data.Context.Mongo.Persistences
+{
+    public class ErroLogPersistence : IErroLogPersistence
+    {
+        private readonly MongoDbContext? _mongoDbContext;
+
+        public ErroLogPersistence(MongoDbContext? mongoDbContext)
+        {
+            _mongoDbContext = mongoDbContext;
+        }
+
+        public void Add(Log model)
+            => _mongoDbContext.GetCollection("LogsErros").InsertOne(model);
+
+        public void Delete(Log model)
+        {
+            var filter = Builders<Log>.Filter.Eq(c => c.Id, model.Id);
+            _mongoDbContext.GetCollection("LogsErros").DeleteOne(filter);
+        }
+
+        public List<Log> GetAll()
+        {
+            var filter = Builders<Log>.Filter.Where(c => true);
+            return _mongoDbContext.GetCollection("LogsErros").Find(filter).ToList();
+        }
+
+        public Log GetbyId(Guid id)
+        {
+            var filter = Builders<Log>.Filter.Eq(c => c.Id, id);
+            return _mongoDbContext.GetCollection("LogsErros").Find(filter).FirstOrDefault();
+        }
+
+        public void Update(Log model)
+        {
+            var filter = Builders<Log>.Filter.Eq(c => c.Id, model.Id);
+            _mongoDbContext.GetCollection("LogsErros").ReplaceOne(filter, model);
+        }
+    }
+}
