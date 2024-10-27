870b23b86f11628cbec30112cb72399f40ff9bb7
pedroferreira <psouza@infisconsultoria.com.br>
2024-10-27 18:36:01 -0300

    feat: criacao controller com endpoint teste para input informacoes no mongodb

---
+++ GenericsLogs/GenericsLogs.API/Controllers/LogController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GenericsLogs.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ArmazenarLogs()
+            => Ok("Logs armazenados com sucesso.");
+    }
+}
