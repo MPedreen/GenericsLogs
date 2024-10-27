1020feb97a91650fed9b4f7ae94a6a1ed099dd06
pedroferreira <psouza@infisconsultoria.com.br>
2024-10-27 18:36:14 -0300

    feat: criacao modelos exceptions

---
+++ GenericsLogs/GenericsLogs.API/Exceptions/AppException.cs
@@ -0,0 +1,9 @@
+﻿namespace GenericsLogs.API.Exceptions
+{
+    public class AppException : Exception
+    {
+        public int StatusCode { get; }
+        public object Value { get; }
+        public AppException() : base() { }
+    }
+}
---
+++ GenericsLogs/GenericsLogs.API/Exceptions/ForbiddenException.cs
@@ -0,0 +1,9 @@
+﻿namespace GenericsLogs.API.Exceptions
+{
+    public class ForbiddenException : Exception
+    {
+        public int StatusCode { get; }
+        public object Value { get; }
+        public ForbiddenException() : base() { }
+    }
+}
