605598d09d181113774f4ffe42aa2d8fb750fe4e
pedroferreira <psouza@infisconsultoria.com.br>
2024-10-27 18:35:37 -0300

    feat: registrando dependencias action e error logs

---
+++ GenericsLogs/GenericsLogs.API/Extensions/DependencyInjectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using GenericsLogs.Domain.Interfaces.Persistences;
+using GenericsLogs.Domain.Interfaces.Services;
+using GenericsLogs.Infra.Data.Context.Mongo;
+using GenericsLogs.Infra.Data.Context.Mongo.Persistences;
+using GenericsLogs.Service;
+
+namespace GenericsLogs.API.Extensions
+{
+    public static class DependencyInjectionExtension
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IActionLogPersistence, ActionLogPersistence>();
+            services.AddTransient<IErroLogPersistence, ErroLogPersistence>();
+            services.AddTransient<IActionLogService, ActionLogService>();
+            services.AddTransient<IErroLogService, ErroLogService>();
+
+            services.Configure<MongoDbSettings>(configuration.GetSection("MongoDbSettings"));
+            services.AddTransient<MongoDbContext>();
+
+            return services;
+        }
+    }
+}
